ea55cc6821015eadeb98943b5b8ca0ad65054c4c
Karolina Jonzén <Karolina.Jonzen@regionvasterbotten.se>
2021-02-26 11:09:08 +0100

    lagt till nuget-paket för automatisk kodgenerering.

--- MedtechInnovationModel/Models/ApplicationUser.cs
+++ MedtechInnovationModel/Models/ApplicationUser.cs
@@ -8,5 +8,13 @@ namespace MedtechInnovationModel.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public string County { get; set; }
+        public string CompanyName { get; set; }
+        public string ProfilePicture { get; set; }
+        public string UserType { get; set; }
+        public string CompanySize { get; set; }
     }
 }
---
+++ MedtechInnovationModel/Models/MedtechInnovationModelDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MedtechInnovationModel.Models
+{
+    public partial class MedtechInnovationModelDbContext : DbContext
+    {
+        public MedtechInnovationModelDbContext()
+        {
+        }
+
+        public MedtechInnovationModelDbContext(DbContextOptions<MedtechInnovationModelDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MedtechInnovationModelDb;Integrated Security=True;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
